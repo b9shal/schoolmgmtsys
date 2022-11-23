db9921ea7b9ccfe411c2c175febec38d1bd87fd0
bishal <bishal.k03@gmail.com>
2022-11-24 00:10:48 +0545

    new

--- routes/role.js
+++ routes/role.js
@@ -20,9 +20,7 @@ router.get("/list", async function(req, res){
     var success = true
     var message = "list success"
     var status = 200
-    const data = await role.findAll({
-      attributes: ["id", "type"]
-    })
+    const data = await role.findAll();
 
     res.status(status).json({
       success,
@@ -33,158 +31,157 @@ router.get("/list", async function(req, res){
   } catch (err) {
     success = false
     message = "list fail"
-    status = 500
-    console.log(err.message)
+    status = 500;
 
-    res.status(status).json({
+    res.json({
       success,
-      message
-    })
-    console.log(err)
+      message,
+      error: err,
+    });
   };
 })
 
 
-router.post("/add", validate, async function(req, res) {
+// router.post("/add", validate, async function(req, res) {
 
-  try {
+//   try {
 
-    var success = true
-    var message = "add success"
-    var status = 200
-    var validationError = null
-    var transaction = null
-    const errors = validationResult(req)
+//     var success = true
+//     var message = "add success"
+//     var status = 200
+//     var validationError = null
+//     var transaction = null
+//     const errors = validationResult(req)
 
-    if(!errors.isEmpty()) {
-      message = "add fail"
-      status = 400
-      success = false
-      validationError = []
-      errors.array().forEach(val => {
-        validationError.push({
-          param: val.param,
-          message: val.msg
-        })
-      })
-    }else {
-      const { type } = await req.body
-      transaction = await models.sequelize.transaction()
+//     if(!errors.isEmpty()) {
+//       message = "add fail"
+//       status = 400
+//       success = false
+//       validationError = []
+//       errors.array().forEach(val => {
+//         validationError.push({
+//           param: val.param,
+//           message: val.msg
+//         })
+//       })
+//     }else {
+//       const { type } = await req.body
+//       transaction = await models.sequelize.transaction()
 
-      await role.create({ type },
-      {
-        transaction
-      }).catch(err => {
-        message = "add fail"
-        status = 500
-        success = false
-        console.log(err);
-      })
-      if(success) {
-        await transaction.commit()
-      }else {
-        await transaction.rollback()
-      }
-    }
-  }catch (err) {
-    success = false
-    message = "add fail"
-    status = 400
-    console.log(err);
-  };
-  res.status(status).json({
-    success,
-    message,
-    validationError
-  })
-});
-
-
-router.patch("/edit/:id", validate, async function(req, res) {
-
-  try {
-
-    var success = true
-    var message = "edit success"
-    var status = 200
-    var validationError = null
-    var transaction = null
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()) {
-      message = "edit fail"
-      status = 400
-      success = false
-      validationError = []
-      errors.array().forEach(val => {
-        validationError.push({
-          param: val.param,
-          message: val.msg
-        })
-      })
-    }else {
-      const id = req.params.id
-      const { type } = await req.body
-      transaction = await models.sequelize.transaction()
-      await role.update({ type },
-      {
-        where: { id }
-      },
-      {
-        transaction
-      }).catch(err => {
-        message = "edit fail"
-        status = 500
-        success = false
-        console.log(err);
-      })
-      if(!success) {
-        await transaction.rollback()
-      }else {
-        await transaction.commit()
-      }
-    }
-  }catch (err) {
-    success = false
-    message = "edit fail"
-    status = 400
-    console.log(err);
-  };
-  res.status(status).json({
-    success,
-    message,
-    validationError
-  })
-});
+//       await role.create({ type },
+//       {
+//         transaction
+//       }).catch(err => {
+//         message = "add fail"
+//         status = 500
+//         success = false
+//         console.log(err);
+//       })
+//       if(success) {
+//         await transaction.commit()
+//       }else {
+//         await transaction.rollback()
+//       }
+//     }
+//   }catch (err) {
+//     success = false
+//     message = "add fail"
+//     status = 400
+//     console.log(err);
+//   };
+//   res.status(status).json({
+//     success,
+//     message,
+//     validationError
+//   })
+// });
 
 
+// router.patch("/edit/:id", validate, async function(req, res) {
 
-router.delete("/delete/:id", async function(req, res){
+//   try {
 
-  try {
-    var success = true
-    var message = "add success"
-    var status = 200
-    const id = req.params.id;
-    await role.destroy({
-      where: {
-        id
-      }
-    });
-    res.status(status).json({
-      success,
-      message
-    });
-  } catch(err) {
-    message = "delete fail"
-    success = false
-    status = 500
-    res.status(status).json({
-      success,
-      message
-    });
-    console.log(err);
-  };
-});
+//     var success = true
+//     var message = "edit success"
+//     var status = 200
+//     var validationError = null
+//     var transaction = null
+//     const errors = validationResult(req)
+
+//     if(!errors.isEmpty()) {
+//       message = "edit fail"
+//       status = 400
+//       success = false
+//       validationError = []
+//       errors.array().forEach(val => {
+//         validationError.push({
+//           param: val.param,
+//           message: val.msg
+//         })
+//       })
+//     }else {
+//       const id = req.params.id
+//       const { type } = await req.body
+//       transaction = await models.sequelize.transaction()
+//       await role.update({ type },
+//       {
+//         where: { id }
+//       },
+//       {
+//         transaction
+//       }).catch(err => {
+//         message = "edit fail"
+//         status = 500
+//         success = false
+//         console.log(err);
+//       })
+//       if(!success) {
+//         await transaction.rollback()
+//       }else {
+//         await transaction.commit()
+//       }
+//     }
+//   }catch (err) {
+//     success = false
+//     message = "edit fail"
+//     status = 400
+//     console.log(err);
+//   };
+//   res.status(status).json({
+//     success,
+//     message,
+//     validationError
+//   })
+// });
+
+
+
+// router.delete("/delete/:id", async function(req, res){
+
+//   try {
+//     var success = true
+//     var message = "add success"
+//     var status = 200
+//     const id = req.params.id;
+//     await role.destroy({
+//       where: {
+//         id
+//       }
+//     });
+//     res.status(status).json({
+//       success,
+//       message
+//     });
+//   } catch(err) {
+//     message = "delete fail"
+//     success = false
+//     status = 500
+//     res.status(status).json({
+//       success,
+//       message
+//     });
+//     console.log(err);
+//   };
+// });
 
 module.exports = router;
